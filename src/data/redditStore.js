2e13a7278a54e46ddc98995b2e4078478511cf76
b1mind <brent@1mindsolutions.com>
2021-01-03 22:09:51 -0700

    🧹 clean up todo/commented code

--- src/data/redditStore.js
+++ src/data/redditStore.js
@@ -2,14 +2,8 @@ import { readable } from 'svelte/store'
 
 export const redditGroup = 'ProgrammerHumor'
 
-//Todo derived fetch address
 const CORS = 'https://cors-anywhere.herokuapp.com/'
 const API = `https://www.reddit.com/r/${redditGroup}/.json`
-
-/* 
-const API = derived(redditGroup, ($redditGroup) => {
-  ;`https://www.reddit.com/r/${$redditGroup}/.json`
-}) */
 
 const isValidImageUrl = (url) => {
   if ((!url && typeof url !== 'string') || url.length === 0) return
@@ -18,7 +12,7 @@ const isValidImageUrl = (url) => {
   if (url.match(allowedExtensions)) return true
 }
 
-//Todo make fetch a function to call on?
+//Todo make fetch a function to call and make derived url?
 export const redditPostData = readable([], async (set) => {
   try {
     const response = await fetch(`${CORS}${API}`)
